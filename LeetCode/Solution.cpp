f7472cc17ef62fc61375e52deb257c11b3f8a048
caozp <godsay1983@gmail.com>
2018-05-11 20:28:52 +0800

    isInterleave

--- LeetCode/Solution.cpp
+++ LeetCode/Solution.cpp
@@ -1019,7 +1019,10 @@ bool Solution::searchMatrix(vector<vector<int>>& matrix, int target)
 bool Solution::isInterleave(string& s1, string& s2, string& s3)
 {
 	int m = s1.size(), n = s2.size();
-	if (m + n != s3.size())return false;
+	if (m + n != s3.size())
+	{
+		return false;
+	}
 	vector<vector<bool>> dp(m + 1);
 	for (int i = 0; i <= m; i++)
 	{
